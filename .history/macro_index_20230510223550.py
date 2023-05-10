38d4ee64c43affd2a6bf022fca718cb8de76f639
xianyunyehe <robortx@outlook.com>
2023-05-10 22:38:28 +0800

    完成了文件读取代码

--- .history/macro_index_20230510223550.py
+++ .history/macro_index_20230510223550.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-# 指定文件夹路径
-folder_path = r'G:\\12_Database\\CMDA_管理层讨论与分析_ALL\\2021\\文本\\文本'
+folder_path = './your/folder/path'
 # 获取文件夹中所有文件名
 file_names = os.listdir(folder_path)
 
